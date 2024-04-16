c3c756cd0680326e3a6024ec229be7c32051dd38
unknown <teteulima@outlook.com>
2024-04-16 13:43:46 -0300

    finishing episode page

--- pages/course/episode/[id].tsx
+++ pages/course/episode/[id].tsx
@@ -5,7 +5,7 @@ import HeaderGeneric from "@/src/components/common/headerGeneric";
 import { useEffect, useState } from "react";
 import courseService, { CourseType } from "@/src/services/courseService";
 import PageSpinner from "@/src/components/common/spinner";
-import { Container } from "reactstrap";
+import { Button, Container } from "reactstrap";
 import ReactPlayer from "react-player";
 
 const EpisodePlayer = function () {
@@ -13,6 +13,9 @@ const EpisodePlayer = function () {
 
     const episodeOrder = parseFloat(router.query.id?.toString() || "");
     const courseId = router.query.courseid?.toString() || "";
+
+    console.log(episodeOrder)
+    console.log(courseId)
 
     const [course, setCourse] = useState<CourseType>();
 
@@ -29,6 +32,14 @@ const EpisodePlayer = function () {
       getCourse();
     }, [courseId]);
 
+    const handleNextEpisode = () => {
+        router.push(`/course/episode/${episodeOrder + 1}?courseid=${courseId}`);
+    };
+
+    const handleLastEpisode = () => {
+        router.push(`/course/episode/${episodeOrder - 1}?courseid=${courseId}`);
+      };
+
     if (course?.episodes == undefined) return <PageSpinner />;
 
     return (
@@ -38,7 +49,7 @@ const EpisodePlayer = function () {
             <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
         </Head>
         <main>
-            <HeaderGeneric logoUrl="/home" btnContent={`Voltar para o curso`} btnUrl={`/courses/${courseId}`} />
+            <HeaderGeneric logoUrl="/home" btnContent={`Voltar para o curso`} btnUrl={`/course/${courseId}`} />
             <Container className="d-flex flex-column align-items-center pt-3 gap-3">
                 <p className={styles.episodeTitle}>
                     {course.episodes[episodeOrder].name}
@@ -47,14 +58,41 @@ const EpisodePlayer = function () {
                 {typeof window == "undefined" ? null : (
                     <ReactPlayer
                     className={styles.player}
+                    controls
                     url={`${
                         process.env.NEXT_PUBLIC_BASEURL
                     }/episodes/stream?videoUrl=${
                         course.episodes[episodeOrder].videoUrl
                     }&token=${sessionStorage.getItem("onebitflix-token")}`}
-                        controls
                     />
                 )}
+                <div className={styles.episodeButtonDiv}>
+                    <Button
+                        className={styles.episodeButton}
+                        disabled={episodeOrder === 0 ? true : false}
+                        onClick={handleLastEpisode}
+                    >
+                        <img
+                        src="/episode/iconArrowLeft.svg"
+                        alt="setaEsquerda"
+                        className={styles.arrowImg}
+                        />
+                    </Button>
+                    <Button
+                        className={styles.episodeButton}
+                        disabled={episodeOrder + 1 === course.episodes.length ? true : false}
+                        onClick={handleNextEpisode}
+                    >
+                        <img
+                            src="/episode/iconArrowRight.svg"
+                            alt="setaDireita"
+                            className={styles.arrowImg}
+                        />
+                    </Button>
+                </div>
+                <p className="text-center pb-4">
+                        {course.episodes[episodeOrder].synopsis}
+                </p>
             </Container>
         </main>
         </>
